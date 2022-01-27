ae418f588cd44bba8632528a26020eff950339f7
chmin1020 <chmin1020@gmail.com>
2022-01-27 20:29:32 +0900

    Login first draft and apply viewBinding in LoginActivity.kt

--- app/src/main/java/com/example/anami/LoginActivity.kt
+++ app/src/main/java/com/example/anami/LoginActivity.kt
@@ -2,10 +2,13 @@ package com.example.anami
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.anami.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
