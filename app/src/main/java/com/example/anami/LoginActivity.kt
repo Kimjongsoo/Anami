9f230dd1fba83148889ccb5bf1220decb74584ff
chmin1020 <chmin1020@gmail.com>
2022-01-30 22:32:57 +0900

    add simple toast message on login

--- app/src/main/java/com/example/anami/LoginActivity.kt
+++ app/src/main/java/com/example/anami/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.anami
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.anami.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -14,6 +15,9 @@ class LoginActivity : AppCompatActivity() {
 
         binding.btnLogin.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
+            var account:String = binding.etAccount.text.toString()
+            if(account == "") account = "default"
+            Toast.makeText(this, account + "님 환영합니다", Toast.LENGTH_SHORT).show()
             startActivity(intent)
         }
     }
