95f5469c553a95548f4b4a15864d5ecb96b56b8f
jongsoo <soohappykjs7@naver.com>
2022-02-01 13:37:11 +0900

    Add tab [1/3]

--- app/src/main/java/com/example/anami/ManageNoteActivity.kt
+++ app/src/main/java/com/example/anami/ManageNoteActivity.kt
@@ -2,10 +2,32 @@ package com.example.anami
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TabHost
 
 class ManageNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manage_note)
+
+        val tabHost: TabHost = findViewById<TabHost>(R.id.tabHost1)
+        tabHost.setup()
+
+        val tab1 = tabHost.newTabSpec("Tab 1")
+        tab1.setIndicator("이번 달")
+        tab1.setContent(R.id.tab1)
+        tabHost.addTab(tab1)
+
+        val tab2 = tabHost.newTabSpec("Tab 2")
+        tab2.setIndicator("저번 달")
+        tab1.setContent(R.id.tab2)
+        tabHost.addTab(tab2)
+
+        val tab3 = tabHost.newTabSpec("Tab 3")
+        tab3.setIndicator("이전 달")
+        tab3.setContent(R.id.tab3)
+        tabHost.addTab(tab3)
+
+        tabHost.currentTab = 0
+
     }
 }
