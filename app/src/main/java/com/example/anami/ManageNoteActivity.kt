a758ed6aed394a12cf5d30bb60d7ef16a877dc4d
jongsoo <soohappykjs7@naver.com>
2022-02-04 22:05:30 +0900

    Add tab[2/3]

--- app/src/main/java/com/example/anami/ManageNoteActivity.kt
+++ app/src/main/java/com/example/anami/ManageNoteActivity.kt
@@ -3,31 +3,19 @@ package com.example.anami
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TabHost
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class ManageNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manage_note)
 
-        val tabHost: TabHost = findViewById<TabHost>(R.id.tabHost1)
-        tabHost.setup()
-
-        val tab1 = tabHost.newTabSpec("Tab 1")
-        tab1.setIndicator("이번 달")
-        tab1.setContent(R.id.tab1)
-        tabHost.addTab(tab1)
-
-        val tab2 = tabHost.newTabSpec("Tab 2")
-        tab2.setIndicator("저번 달")
-        tab1.setContent(R.id.tab2)
-        tabHost.addTab(tab2)
-
-        val tab3 = tabHost.newTabSpec("Tab 3")
-        tab3.setIndicator("이전 달")
-        tab3.setContent(R.id.tab3)
-        tabHost.addTab(tab3)
-
-        tabHost.currentTab = 0
+        val pagerAdapter = ManageFragmentAdapter(supportFragmentManager)
+        val pager = findViewById<ViewPager>(R.id.viewPager)
+        pager.adapter = pagerAdapter
+        val tab = findViewById<TabLayout>(R.id.tab)
+        tab.setupWithViewPager(pager)
 
     }
 }
