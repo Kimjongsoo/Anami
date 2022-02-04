39b105dec29ae5bc3c4d9b64b8b5ece54e33eb8c
chmin1020 <chmin1020@gmail.com>
2022-02-04 16:45:30 +0900

    develop function of button

--- app/src/main/java/com/example/anami/ChangeProfileActivity.kt
+++ app/src/main/java/com/example/anami/ChangeProfileActivity.kt
@@ -2,6 +2,7 @@ package com.example.anami
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.anami.databinding.ActivityChangeProfileBinding
 
 class ChangeProfileActivity : AppCompatActivity() {
@@ -10,5 +11,10 @@ class ChangeProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityChangeProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnChangeInfo.setOnClickListener {
+            Toast.makeText(this, "수정되었습니다.", Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 }
